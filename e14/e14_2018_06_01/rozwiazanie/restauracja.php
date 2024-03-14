ab6c96cfa56d1a3574fe08e0ac2b72f5b822ff9b
marekgebski <marekgebski@gmail.com>
2024-03-14 13:14:26 +0100

    e14_2018_06_01 php

--- e14/e14_2018_06_01/rozwiazanie/restauracja.php
+++ e14/e14_2018_06_01/rozwiazanie/restauracja.php
@@ -1,12 +1,19 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'baza');
-if(!empty($_POST['data']) && !empty($_POST['osob']) && !empty($_POST['telefon'])) {
+if (!empty($_POST['data']) && !empty($_POST['osob']) && !empty($_POST['telefon'])) {
+	$id_polaczenia = mysqli_connect('localhost', 'root', '', 'baza');
 	$data = $_POST['data'];
 	$osob = $_POST['osob'];
 	$telefon = $_POST['telefon'];
-	$kw = "INSERT INTO rezerwacje(id, data_rez, liczba_osob, telefon) VALUES (NULL, '$data', $osob, '$telefon');";
-	mysqli_query($con, $kw);
+	$insert = <<<KONIEC
+	INSERT INTO rezerwacje(
+			id,
+			data_rez,
+			liczba_osob,
+			telefon
+		)
+		VALUES(NULL, '$data', $osob, '$telefon');
+	KONIEC;
+	mysqli_query($id_polaczenia, $insert);
 	echo "Dodano rezerwacje do bazy";
+	mysqli_close($id_polaczenia);
 }
-mysqli_close($con);
-?>
