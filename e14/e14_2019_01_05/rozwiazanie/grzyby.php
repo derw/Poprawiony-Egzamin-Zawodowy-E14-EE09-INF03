76c0b9dc356c5c9f98db3ff79dbaca2c2255ffa7
marekgebski <marekgebski@gmail.com>
2024-03-18 16:21:59 +0100

    e14_2019_01_05

--- e14/e14_2019_01_05/rozwiazanie/grzyby.php
+++ e14/e14_2019_01_05/rozwiazanie/grzyby.php
@@ -1,50 +1,92 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="UTF-8" />
+	<meta charset="UTF-8">
 	<title>Grzybobranie</title>
-	<link rel="stylesheet" href="styl5.css" />
+	<link rel="stylesheet" href="styl5.css">
 </head>
+
 <body>
 	<div id="miniatura">
-		<a href="borowik.jpg"><img src="borowik-miniatura.jpg" alt="Grzybobranie" /></a>
+		<a href="borowik.jpg"><img src="borowik-miniatura.jpg" alt="Grzybobranie"></a>
 	</div>
 	<div id="tytulowy">
 		<h1>Idziemy na grzyby</h1>
 	</div>
 	<div id="lewy">
 		<?php
-		$con = mysqli_connect('localhost', 'root', '', 'dane2');
-		$kw1 = "SELECT nazwa_pliku, potoczna FROM grzyby;";
-		$res1 = mysqli_query($con, $kw1);
-		while($tab = mysqli_fetch_row($res1)) {
-			echo "<img src='$tab[0]' title='$tab[1]' />";
+		$id_polaczenia = mysqli_connect('localhost', 'root', '', 'dane2');
+		$zapytanie = <<<KONIEC
+		SELECT
+			`potoczna`,
+			`nazwa_pliku`
+		FROM
+			`grzyby`
+		KONIEC;
+		$wynik = mysqli_query($id_polaczenia, $zapytanie);
+		while ($row = mysqli_fetch_array($wynik)) {
+			$potoczna = $row['potoczna'];
+			$obrazek = $row['nazwa_pliku'];
+			echo <<<KONIEC
+			<img src='$obrazek' title='$potoczna'>
+			KONIEC;
 		}
 		?>
 	</div>
 	<div id="prawy">
 		<h2>Grzyby jadalne</h2>
 		<?php
-		$kw2 = "SELECT nazwa, potoczna FROM grzyby WHERE jadalny = 1;";
-		$res2 = mysqli_query($con, $kw2);
-		while($tab = mysqli_fetch_row($res2)) {
-			echo "<p>$tab[0] ($tab[1])</p>";
+		$zapytanie = <<<KONIEC
+		SELECT
+			nazwa,
+			potoczna
+		FROM
+			grzyby
+		WHERE
+			jadalny = 1;
+		KONIEC;
+		$wynik = mysqli_query($id_polaczenia, $zapytanie);
+		while ($row = mysqli_fetch_array($wynik)) {
+			$nazwa = $row['nazwa'];
+			$potoczna = $row['potoczna'];
+			echo <<<KONIEC
+			<p>$nazwa ($potoczna)</p>
+			KONIEC;
 		}
 		?>
 		<h2>Polecamy do sosów</h2>
 		<?php
-		$kw3 = "SELECT grzyby.nazwa, grzyby.potoczna, rodzina.nazwa FROM grzyby, rodzina, potrawy WHERE grzyby.rodzina_id = rodzina.id AND grzyby.potrawy_id = potrawy.id AND potrawy.nazwa = 'sos';";
-		$res3 = mysqli_query($con, $kw3);
+		$zapytanie = <<<KONIEC
+		SELECT
+			g.nazwa,
+			g.potoczna,
+			r.nazwa AS rodzina
+		FROM
+			grzyby AS g
+		JOIN rodzina AS r
+		ON
+			g.rodzina_id = r.id
+		WHERE
+			g.potrawy_id = 1
+		KONIEC;
+		$wynik = mysqli_query($id_polaczenia, $zapytanie);
 		echo "<ol>";
-		while($tab = mysqli_fetch_row($res3)) {
-			echo "<li>$tab[0] ($tab[1]), rodzina: $tab[2]</li>";
+		while ($row = mysqli_fetch_array($wynik)) {
+			$nazwa = $row['nazwa'];
+			$potoczna = $row['potoczna'];
+			$rodzina = $row['rodzina'];
+			echo <<<KONIEC
+			<li>$nazwa ($potoczna), rodzina: $rodzina</li>
+			KONIEC;
 		}
 		echo "</ol>";
-		mysqli_close($con);
+		mysqli_close($id_polaczenia);
 		?>
 	</div>
 	<div id="stopka">
 		<p>Autor: PESEL</p>
 	</div>
 </body>
+
 </html>
