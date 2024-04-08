31763b6e9dc77f5104ebcd8217eba9de50cd1466
marekgebski <marekgebski@gmail.com>
2024-04-08 11:01:13 +0200

    e14_2019_06_02 - PHP

--- e14/e14_2019_06_02/rozwiazanie/index.php
+++ e14/e14_2019_06_02/rozwiazanie/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="UTF-8" />
+	<meta charset="UTF-8">
 	<title>Hurtownia papeirnicza</title>
-	<link rel="stylesheet" href="styl.css" />
+	<link rel="stylesheet" href="styl.css">
 </head>
+
 <body>
 	<div id="baner">
 		<h1>W naszej hurtowni kupisz najtaniej</h1>
@@ -12,14 +14,28 @@
 	<div id="lewy">
 		<h3>Ceny wybranych artykułów w hurtowni:</h3>
 		<table>
-		<?php
-		$con = mysqli_connect('localhost', 'root', '', 'sklep');
-		$kw1 = "SELECT nazwa, cena FROM towary LIMIT 4;";
-		$res1 = mysqli_query($con, $kw1);
-		while($tab = mysqli_fetch_row($res1)) {
-			echo "<tr><td>$tab[0]</td><td>$tab[1] zł</td></tr>";
-		}
-		?>
+			<?php
+			$id_polaczenia = mysqli_connect('localhost', 'root', '', 'sklep');
+			$zapytanie = <<<KONIEC
+				SELECT
+					`nazwa`,
+					`cena`
+				FROM
+					`towary`
+				LIMIT 4;
+			KONIEC;
+			$wynik = mysqli_query($id_polaczenia, $zapytanie);
+			while ($row = mysqli_fetch_array($wynik)) {
+				$nazwa = $row['nazwa'];
+				$cena = $row['cena'];
+				echo <<<KONIEC
+				<tr>
+					<td>$nazwa</td>
+					<td>$cena zł</td>
+				</tr>
+				KONIEC;
+			}
+			?>
 		</table>
 	</div>
 	<div id="srodkowy">
@@ -34,40 +50,47 @@
 					<option value="Linijka 30 cm">Linijka 30 cm</option>
 					<option value="Ekierka">Ekierka</option>
 					<option value="Linijka 50 cm">Linijka 50 cm</option>
-				</select><br/>
+				</select><br>
 			</label>
 			<label>
 				liczba sztuk:
-				<input type="number" name="liczba" value="1"/>
+				<input type="number" name="liczba" value="1" />
 			</label>
-			<br/>
+			<br>
 			<button type="submit">OBLICZ</button>
 		</form>
 		<?php
-		if(!empty($_POST['lista']) && !empty($_POST['liczba'])) {
+		if (!empty($_POST['lista']) && !empty($_POST['liczba'])) {
 			$produkt = $_POST['lista'];
 			$liczba = $_POST['liczba'];
-			$kw2 = "SELECT cena FROM towary WHERE nazwa = '$produkt';";
-			$res2 = mysqli_query($con, $kw2);
-			$tab = mysqli_fetch_row($res2);
-			$cena = ROUND($tab[0] * $liczba, 1);
-			echo "$cena";
+			$zapytanie = <<<KONIEC
+			SELECT
+				`cena`
+			FROM
+				`towary`
+			WHERE
+				nazwa = '$produkt';
+			KONIEC;
+			$wynik = mysqli_query($id_polaczenia, $zapytanie);
+			$row = mysqli_fetch_array($wynik);
+			echo ROUND($row['cena'] * $liczba, 1);
 		}
-		mysqli_close($con);
+		mysqli_close($id_polaczenia);
 		?>
 	</div>
 	<div id="prawy">
-		<img src="zakupy2.png" alt="hurtownia" />
+		<img src="zakupy2.png" alt="hurtownia">
 		<h3>Kontakt</h3>
 		<p>
-		telefon: <br/>
-		111222333<br/>
-		e-mail: <br/>
-		<a href="mailto:hurt@wp.pl">hurt@wp.pl</a>
+			telefon: <br>
+			111222333<br>
+			e-mail: <br>
+			<a href="mailto:hurt@wp.pl">hurt@wp.pl</a>
 		</p>
 	</div>
 	<div id="stopka">
 		<h4>Witrynę wykonał PESEL</h4>
 	</div>
 </body>
+
 </html>
